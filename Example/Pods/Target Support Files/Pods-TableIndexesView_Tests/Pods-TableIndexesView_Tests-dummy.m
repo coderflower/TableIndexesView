41b588127103466f8e99d9ef94e6cc48525e3d3b
蔡龙君 <cailongjun@huami.com>
2020-05-09 16:01:20 +0800

    初始化工程

---
+++ Example/Pods/Target Support Files/Pods-TableIndexesView_Tests/Pods-TableIndexesView_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TableIndexesView_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_TableIndexesView_Tests
+@end
---
+++ Example/Pods/Target Support Files/TableIndexesView/TableIndexesView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_TableIndexesView : NSObject
+@end
+@implementation PodsDummy_TableIndexesView
+@end
